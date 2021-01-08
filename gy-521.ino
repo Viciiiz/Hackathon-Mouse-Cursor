eab811f2e9b125c1461abe72fe228b941cb6da77
Victor Andrianarivo Rakotondranoro <71042541+Viciiiz@users.noreply.github.com>
2021-01-07 19:11:31 -0600

    Arduino program to read data from the gy-521 board

---
+++ gy-521.ino
@@ -0,0 +1,41 @@
+/**
+ * Read data from the gy-521 board. The program will return the X rotation, the Y rotation, a boolean for clicking flag, and a boolean for control flag.
+ */
+
+#include "Wire.h" // I2C communication
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t x, y; // steps
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the GY-521 board
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  
+  // print out data
+  Serial.print("aX = "); 
+  Serial.print(convert_int16_to_str(x));
+  Serial.print(" | aY = "); 
+  Serial.println(convert_int16_to_str(y));
+
+  // delay
+  delay(1000);
+}
